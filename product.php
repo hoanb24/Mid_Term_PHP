217e8e4f1e93e2ffb871ddf05a2f366af05b869b
hoanb24 <115441410+hoanb24@users.noreply.github.com>
2023-03-03 09:45:38 +0700

    Add files via upload

--- product.php
+++ product.php
@@ -6,6 +6,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+<link rel="shortcut icon" href="https://cf.shopee.vn/file/51460c45fc3b4ee731fb18a73e706135" type="image/x-icon">
 <style>
     img {
         width: 200px;
